e89172f18d57bd78dc37de177afba6ec586889c8
Alfredo A <68718651+alfredojavier55@users.noreply.github.com>
2023-08-10 20:50:11 -0300

    Update SIVRP_se.R

--- Sensibility/SIVRP_se.R
+++ Sensibility/SIVRP_se.R
@@ -1,13 +1,22 @@
-SIRVP_se <- function (beta, gama, tau, mu, w, theta, tf)
+SIRVP_se <- function (beta, betab, gama, mu, w, theta, tau, d, r, tf)
   {
 # Parameters----
-#Montly parameters ----
-# beta <- 4.0706 #Calculada nls
-# gama <- 2.7941 #Calculada nls
-# tau <- gama/30 #
-# mu <- (1/(350/30))
-# w <- 1/(180/30) #omega
-# theta <- 0.1
+#Monthly parameters ----
+
+beta <- 1.1664 #best
+# beta <- 4.0706 # Calculated nls monthly
+betab <- beta * 0.4 # Assumption
+gama <- 0.6937 #best
+# gama <- 2.7941 # Calculated nls monthly                
+mu <- (1 / (350 / 30)) # natural death rate
+w <- 1 / (180 / 30) # omega loss of vaccine induced immunity
+theta <- 0.15 # percentage of animals persistent infected
+tau <- gama / 30 # death rate of persistent infected
+# d <- (-30*log(0.001))/7  # removal of carcass 7 days
+# r <- (-30*log(0.001))/1  # removal of carcass 7 days
+d <- (1/(10/30))  # Monthly number of days to remove the carcass
+r <- (1/(15/30))  # Monthly number of days to natural decay of carcass to be used as swill
+
 
 # Coverage ----
 rnorm2 <- function(n,mean,sd) { mean+sd*scale(rnorm(n)) }
@@ -23,50 +32,65 @@ vc[vc > .75,] <- .75
 # vaccine coverage function by years  ----
 c <- vc
 # cf coverage function
-fp <- function(t){
-  {fp <- c[t]}
-  return(fp)
+fc <- function(t){
+  c[t]
 }
 
-# Parametros ----
-par.SIRVsd <- c(beta = beta, gama = gama, mu = mu, w = w, theta = theta, tau = tau)
+fc(10)
+
+# Parameters ----
+par.SIRVsd <- c(beta = beta,
+                betab = betab,
+                gama = gama,
+                mu = mu,
+                w = w,
+                theta = theta,
+                tau = tau,
+                d = d,
+                r = r)
 
 # Define system ----
 # Initial state vector ----
 # calculating number of animals by S, I and V ----
 pop <- 2000000
-infe <-208 #136 #final conditions  Dec-2021
-p_infe <- 520 #413 #persistent infected
-r <- infe+p_infe
-sus <- pop-infe-p_infe
+infe <- round(((551/12 * 100) / 6), 0) # 770/12
+p.i <- round(infe * 0.5, 0) # persistent infected
+# infe <-10 #136 #final conditions  Dec-2021
+# p_infe <- 20 #413 #persistent infected
+sus <- pop-infe-p.i
+car <- (infe + p.i) * 0.5
 
 # 80%
-vac <-(sus)*(0.75) #initial vaccinated pop as jan 2022
+vac <-(sus)*(0.75)*0.95 #initial vaccinated pop as jan 2022
+sus <- pop-vac-infe-p.i
 
-sus <- pop-vac-r
-vac+sus+r-pop
+infe + p.i + vac + sus -pop
 
-#  Set intial vector
-init.sd <- c(S=sus, I=infe, PI=p_infe, V=vac, R=r)                      
+#  Set initial vector
+init.sd <- c(S=sus,
+             I=infe,
+             P=p.i, 
+             V=vac, 
+             C=car)                      
 
-# Deterministic modell  ----
+# Deterministic model  ----
 # Calling function
 # Time interval 2014-2022  ----
 Dt <- 1
-tf <- 12*2   # Final time 2022-2033
+tf <- 12*1   # Final time 2022-2033
 
 # SIRV with population dynamics  ----
 SIRVsd <- function(t, state, parameters){
   with(as.list(c(state, parameters)),{
     
     # Rate of change
-    dV = (fp(t))*mu*(S+I+PI+V+R) - w*V - mu*V
-    dS = (1-(fp(t)))*mu*(S+I+PI+V+R) - (beta*S*I)/(S+I+PI+V) + w*V - mu*S
-    dI = (beta*S*I)/(S+I+PI+V+R) - gama*I*(1-theta) - gama*I*theta - mu*I
-    dPI= gama*I*theta - tau*PI - mu*PI
-    dR= gama*I*(1-theta) + tau*PI - mu*R
+    dV <- fc(t)*mu*(V+S+I+P+C) - w*mu*V - mu*V -(betab*V*C)/(V+S+I+P+C)
+    dS <- (1 - fc(t))*mu*(V+S+I+P+C) + w*mu*V - (beta*S*I)/(V+S+I+P+C) - mu*S - (betab*C*S)/(V+S+I+P+C)
+    dI <- (beta*S*I)/(V+S+I+P+C) - (1-theta)*gama*I - theta*gama*I + (betab*C*S)/(V+S+I+P+C) - mu * I
+    dP <- theta*gama*I - tau*P - mu*P + (betab*V*C)/(V+S+I+P+C)
+    dC <- tau*P + (1-theta)*gama*I - d*C - r*C
     # Return the output of the model
-    return(list(c(dS, dI, dPI, dV, dR)))
+    return(list(c(dS, dI, dP, dV, dC)))
   })
 }
 
@@ -74,8 +98,11 @@ times <- seq(1, tf, by=Dt)
 
 # Simulation ----
 modSIRVsd <- ode(y = init.sd, times = times, func = SIRVsd, parms = par.SIRVsd, method = "ode45")
+# matplot.0D(modSIRVsd, which = c("I", "P", "C"), lwd = 2)
+# plot(modSIRVsd)
 modSIRVsd <- as.data.frame(modSIRVsd)
-modSIRVsd$N <- (modSIRVsd$S + modSIRVsd$I + modSIRVsd$PI + modSIRVsd$V + modSIRVsd$R)
-modSIRVsd$p <- modSIRVsd$I+modSIRVsd$PI
+modSIRVsd$N <- (modSIRVsd$S + modSIRVsd$I + modSIRVsd$P + modSIRVsd$V + modSIRVsd$C)
+modSIRVsd$In <- modSIRVsd$I+modSIRVsd$P
+modSIRVsd$pre <- modSIRVsd$In/modSIRVsd$N
 modSIRVsd <- as.data.frame(modSIRVsd)
 }
