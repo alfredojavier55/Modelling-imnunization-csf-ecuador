3e51f2926f6be9c991cddb99ea4d97969bae6955
Alfredo A <68718651+alfredojavier55@users.noreply.github.com>
2023-08-10 20:48:48 -0300

    Update code

--- Sim22-32/2022-2032_montly_SIVRP.R
+++ Sim22-32/2022-2032_montly_SIVRP.R
@@ -1,59 +1,124 @@
 ###### 2014-2022 S-I-PI-V Simulation ######----
-###### 2014-20 S-I-P-V Simulation ######----
 
-library(zoo); library(deSolve); library(ggplot2); library(lubridate); library(dplyr)
-# Diretorio de trabalho
-setwd("~/Dropbox/0.USP/7.Publicações/Mathematical modelling CSFV/Analise/")
+library(zoo);
+library(deSolve);
+library(ggplot2);
+library(lubridate);
+library(dplyr)
+setwd("~/papers/modelling_csf/Modelling-imnunization-csf-ecuador/Sim22-32/")
 
-# Parametros - taxas  ###----
-#taxas em dias ----
-beta <- 4.0706 #Calculada nls
-gama <- 2.7941 #Calculada nls
-tau <- gama/30 #
-mu <- (1/(350/30))
+# Parameters ----
+beta <- 1.1664 # Calculated nls monthly
+betab <- 4.0706 * 0.5 # Assumption
+gama <- 0.6939 # Calculated nls monthly                
+mu <- (1 / (350 / 30)) # natural death rate
+w <- 1 / (180 / 30) # omega loss of vaccine induced immunity
+theta <- 0.35 # percentage of animals persistent infected
+tau <- gama / 30 # death rate of persistent infected
+# d <- (-30*log(0.001))/7  # removal of carcass 7 days
+# r <- (-30*log(0.001))/1  # removal of carcass 7 days
+
+d <- (1/(5/30))  # Monthly number of days to remove the carcass
+r <- (1/(1/30))  # Monthly number of days to natural decay of carcass to be used as swill
+
 R0 <- beta/gama
-w <- 1/(180/30) #omega
-theta <- 0.1
 
-# About tau Persisten infected parameter 
-# 1/(gama/30)
-# 45 semanas / 4 semanas por mes = 11.25 meses 
-# 1/(45/4) = 0.0888
+# Parameters ----
+par.SIRVsd <- c(beta = beta,
+                betab = betab,
+                gama = gama,
+                mu = mu,
+                w = w,
+                theta = theta,
+                tau = tau,
+                d = d,
+                r = r)
+# Define system ----
+# Initial state vector ----
+# calculating number of animals by S, I and V ----
+pop <- 2000000
+
+# Actualizar con la informacion de casos de PPC ----
+infe <- round(((551/12 * 100) / 6), 0) # 770/12
+p.i <- round(infe * 0.5, 0) # persistent infected
+car <- (infe + p.i) * 0.5
+sus <- pop-infe-p.i-car
+
+# 80%
+mean(coverage$vac_cov[64:70]) # Coverage of the last semester of 2021
+vac <-(sus)*(0.85)*0.95 #initial vaccinated pop as jan 2022
+sus <- pop-vac-infe-p.i-car
+infe + p.i + vac + sus + car - pop
+vac/(pop)
+
+#  Set initial vector
+init.sd <- c(S=sus, 
+             I=infe, 
+             P=p.i, 
+             V=vac,
+             C=car)                      
+init.sd
+
+# Deterministic model  ----
+# Calling function
+# Time interval 2014-2022  ----
+Dt <- 1
+tf <- 12*10   # Final time 2022-2033
+times <- seq(1, tf, by=Dt)
+
+# SIRV with population dynamics  ----
+SIRVsd <- function(t, state, parameters) {
+  with(as.list(c(state, parameters)), {
+    # rate of change
+
+    dV <- fc(t)*mu*(V+S+I+P+C) - w*mu*V - mu*V -(betab*V*C)/(V+S+I+P+C)
+    dS <- (1 - fc(t))*mu*(V+S+I+P+C) + w*mu*V - (beta*S*I)/(V+S+I+P+C) - mu*S - (betab*C*S)/(V+S+I+P+C)
+    dI <- (beta*S*I)/(V+S+I+P+C) - (1-theta)*gama*I - theta*gama*I + (betab*C*S)/(V+S+I+P+C) - mu * I
+    dP <- theta*gama*I - tau*P - mu*P + (betab*V*C)/(V+S+I+P+C)
+    dC <- tau*P + (1-theta)*gama*I - d*C - r*C
+    
+        # return the output of the model
+    return(list(c(dS, dI, dP, dV, dC)))
+  })
+}
 
 # Coverage ----
-
-# v <- .9
-# vc <- c(rep(c(v*.95, v*1.05),60))
-# runif(120, 0.9025, 7)
-
 rnorm2 <- function(n,mean,sd) { mean+sd*scale(rnorm(n)) }
 
-#95%
-vc <- rnorm2(120,.95,.00625)
-vc[vc > .95,] <- .95
+#50%
+vc <- rnorm2(120,.50,.00625)
+vc[vc > .5,] <- .5
+
+#60%
+vc <- rnorm2(120,.60,.00625)
+vc[vc > .6,] <- .6
+
+# # #70%
+vc <- rnorm2(120,.70,.00625)
+vc[vc > .7,] <- .7
+
+# # #75%
+vc <- rnorm2(120,.75,.00625)
+vc[vc > .75,] <- .75
+
+# #80%
+vc <- rnorm2(120,.80,.00625)
+vc[vc > .8,] <- .8
+
+# #85%
+vc <- rnorm2(120,.85,.00625)
+vc[vc > .85,] <- .85
 
 #90%
 vc <- rnorm2(120,.9,.00625)
 vc[vc > .9,] <- .9
 
-#85%
-vc <- rnorm2(120,.85,.00625)
-vc[vc > .85,] <- .85
+# #95%
+vc <- rnorm2(120,.95,.00625)
+vc[vc > .95,] <- .95
 
-#80%
-vc <- rnorm2(120,.80,.00625)
-vc[vc > .8,] <- .8
-
-#75%
-vc <- rnorm2(120,.75,.00625)
-vc[vc > .75,] <- .75
-
-#70%
-vc <- rnorm2(120,.70,.00625)
-vc[vc > .7,] <- .7
 
 mean(vc); summary(vc)
-
 # vaccine coverage function by years  ----
 c <- vc
 # cf coverage function
@@ -62,62 +127,21 @@ fc <- function(t){
 }
 
 fc(120) #testing
-
-# Parametros ----
-par.SIRVsd <- c(beta = beta, gama = gama, mu = mu, w = w, theta = theta, tau = tau)
-
-# Define system ----
-# Initial state vector ----
-# calculating number of animals by S, I and V ----
-pop <- 2000000
-infe <-208 #136 #final conditions  Dec-2021
-p_infe <- 520 #413 #persistent infected
-r <- infe+p_infe
-sus <- pop-infe-p_infe
-
-# 80%
-vac <-(sus)*(0.8) #initial vaccinated pop as jan 2022
-
-sus <- pop-vac-r
-vac+sus+r-pop
-
-#  Set intial vector
-init.sd <- c(S=sus, I=infe, P=p_infe, V=vac, R=r)                      
-
-# Deterministic modell  ----
-# Calling function
-# Time interval 2014-2022  ----
-Dt <- 1
-tf <- 12*10   # Final time 2022-2033
-
-# SIRV with population dynamics  ----
-SIRVsd <- function(t, state, parameters){
-  with(as.list(c(state, parameters)),{
-    
-    # rate of change
-    dV = (fc(t))*mu*(S+I+P+V+R) - w*V - mu*V
-    dS = (1-(fc(t)))*mu*(S+I+P+V+R) - (beta*S*I)/(S+I+P+V) + w*V - mu*S
-    dI = (beta*S*I)/(S+I+P+V+R) - gama*I*theta - gama*I*(1-theta) - mu*I
-    dP= gama*I*(1-theta) - tau*P - mu*P
-    dR= gama*I*theta + tau*P - mu*R
-    # return the output of the model
-    return(list(c(dS, dI, dP, dV, dR)))
-  })
-}
-
-times <- seq(1, tf, by=Dt)
-
 # Simulation ----
 modSIRVsd <- ode(y = init.sd, times = times, func = SIRVsd, parms = par.SIRVsd, method = "ode45")
 modSIRVsd <- as.data.frame(modSIRVsd)
-modSIRVsd$N <- (modSIRVsd$S + modSIRVsd$I + modSIRVsd$P + modSIRVsd$V + modSIRVsd$R)
+modSIRVsd$N <- (modSIRVsd$S + modSIRVsd$I + modSIRVsd$P + modSIRVsd$V)
+modSIRVsd$coverage <- modSIRVsd$V/modSIRVsd$N
 modSIRVsd$month <- ymd("2022-01-01")+ months(1:120)
-modSIRVsd$I[modSIRVsd$I <=1 ] <- 0
-modSIRVsd$P[modSIRVsd$P <=1 ] <- 0
+modSIRVsd$I[modSIRVsd$I <=0.5 ] <- 0
+modSIRVsd$P[modSIRVsd$P <=0.5 ] <- 0
+modSIRVsd$C[modSIRVsd$C <=0.5 ] <- 0
 modSIRVsd$infected <- (modSIRVsd$I + modSIRVsd$P)
-modSIRVsd$I[modSIRVsd$I <=1 ] <- NA
-modSIRVsd$P[modSIRVsd$P <=1 ] <- NA
+modSIRVsd$I[modSIRVsd$I == 0 ] <- NA
+modSIRVsd$P[modSIRVsd$P == 0 ] <- NA
+modSIRVsd$C[modSIRVsd$C == 0 ] <- NA
 
+compare <- modSIRVsd
 
 # Calculating the prevalence 
 # Prevalence
@@ -136,12 +160,12 @@ modSIRVsd$month[which.min(modSIRVsd$I)]
 modSIRVsd$month[which.min(modSIRVsd$P)]
 
 # Creating a df with all simulation results
-# modresults <- data.frame(modSIRVsd$month)
-# modresults$I95 <- modSIRVsd$I
-# modresults$P95 <- modSIRVsd$P
- 
-# modresults$I90 <- modSIRVsd$I
-# modresults$P90 <- modSIRVsd$P
+modresults <- data.frame(modSIRVsd$month)
+modresults$I95 <- modSIRVsd$I
+modresults$P95 <- modSIRVsd$P
+#
+modresults$I90 <- modSIRVsd$I
+modresults$P90 <- modSIRVsd$P
 
 modresults$I85 <- modSIRVsd$I
 modresults$P85 <- modSIRVsd$P
@@ -155,11 +179,13 @@ modresults$P75 <- modSIRVsd$P
 modresults$I70 <- modSIRVsd$I
 modresults$P70 <- modSIRVsd$P
 
+modresults$I60 <- modSIRVsd$I
+modresults$P60 <- modSIRVsd$P
 
-modresults$x <- modSIRVsd$I
-modresults$Px <- modSIRVsd$P
+modresults$I50 <- modSIRVsd$I
+modresults$P50 <- modSIRVsd$P
 
-# Montly mean of animals by status I or IP
+# Monthly mean of animals by status I or IP
 mean(modresults$I95[1:4])
 mean(modresults$P95[1:35])
 
@@ -180,61 +206,98 @@ library(ggplot2); library(lubridate)
 library(reshape2)
 df <- melt(modresults, id="modSIRVsd.month")
 
+# Mutating names
+df$variable <- paste(df$variable, "%")
+table(df$variable)
+
 # General simulations
 ggplot(df, aes(floor_date(ymd(modSIRVsd.month), unit = "month")))+ 
-  geom_line(aes(y=value, colour=variable), size=2)+
+  geom_line(aes(y=value, colour=variable), alpha=0.5, size=1,
+            position=position_dodge2(width=2,
+                                    padding=1.5,
+                                    preserve = "single"))+
   scale_colour_viridis_d()+
+  scale_y_continuous(trans = "sqrt") +
   ylab('Population')+
   xlab('Time (months)')+
-  ylim(0, 14000)+
+  # ylim(0, 100)+
   labs(colour = "Infected
 Simulation 
 dynamics")+
   theme_linedraw()
 
-
-# Mutating names
-df <- df %>% 
-  mutate(simulation=ifelse(variable == "I95" | variable == "P95","90-95%",NA)) %>% 
-  mutate(simulation=ifelse(variable == "I90" | variable == "P90","90-95%",simulation)) %>% 
-  mutate(simulation=ifelse(variable == "I85" | variable == "P85","85%",simulation)) %>% 
-  mutate(simulation=ifelse(variable == "I80" | variable == "P80","80%",simulation)) %>% 
-  mutate(simulation=ifelse(variable == "I75" | variable == "P75","75%",simulation)) %>% 
-  mutate(simulation=ifelse(variable == "I70" | variable == "P70","70%",simulation))
-
-table(df$simulation)
-
 # Grouping
-df$group <- rep(c(rep("Infected",120), rep("Persistent I",120)), 6)
+df$group <- rep(c(rep("Infected",120), rep("Persistent I",120)), 8)
+df$group <- rep(c(rep("Infected",120), rep("Persistent I",120)), 5)
+df$perc <- substr(df$variable,2,5)
+
 
 # last plot  ----
 df %>% 
-  ggplot(aes(floor_date(ymd(modSIRVsd.month), unit = "month")))+ 
-  geom_line(aes(y=value, colour=variable), size=1, alpha=0.2)+
-  geom_point(aes(y=value, colour=variable, shape=factor(group)), size=1.5, alpha=0.7)+
-  scale_colour_viridis_d(direction=-1)+
+  filter(variable != "I70 %") %>% 
+  filter(variable != "P70 %") %>% 
+  filter(variable != "I85 %") %>% 
+  filter(variable != "P85 %") %>% 
+  filter(variable != "I75 %") %>% 
+  filter(variable != "P75 %") %>% 
+  filter(variable != "I60 %") %>%
+  filter(variable != "P60 %") %>%
+  ggplot(aes(floor_date(ymd(modSIRVsd.month), unit = "month"))) + 
+  geom_line(aes(y=value, colour=variable), size=2, alpha=1) +
+  geom_point(aes(y=value, colour=variable, shape=factor(group)), size=2, alpha=1) +
+  scale_colour_viridis_d(direction=-1) +
   scale_shape_manual(values = c(46, 20)) +
-  facet_grid(rows = "simulation", scales="free")+
-  ylab('Simulated infected population')+
-  xlab(" ") +
+  facet_grid(rows = "variable", scales = "free") +
+  ylab('Simulated infected population') +
+  xlab(NULL)+
   labs(shape = "Infected
 Simulation 
-dynamics")+
-  theme_linedraw()+
-  guides(colour="none")
+dynamics") +
+  theme_linedraw() +
+  guides(colour="none") +
+  guides(shape="none")
+
 
 write.csv(df, file = "df.csv")
+class(df$modSIRVsd.month)
 
-df$per <- c(rep(95,240), rep(90,240), rep(85,240), rep(80,240), rep(75,240), rep(70,240))
+df <- read.csv(file="df.csv")
+df$modSIRVsd.month <- date(df$modSIRVsd.month)
+
+df %>% 
+  filter(variable != "I70 %") %>% 
+  filter(variable != "P70 %") %>% 
+  filter(variable != "I85 %") %>% 
+  filter(variable != "P85 %") %>% 
+  filter(variable != "I75 %") %>% 
+  filter(variable != "P75 %") %>% 
+  filter(variable != "I60 %") %>% 
+  filter(variable != "P60 %") %>% 
+  ggplot(aes(floor_date(ymd(modSIRVsd.month), unit = "month"))) + 
+  geom_line(aes(y=value, colour=variable), size=2, alpha=1) +
+  scale_colour_viridis_d(direction=-1) +
+  facet_grid(rows = "variable", scales = "free") +
+  ylab('Simulated infected population') +
+  xlab(NULL)+
+  theme_linedraw() +
+  guides(colour="none") +
+  guides(shape="none")
+
+
 
 # Montly prevalence (%)
 df %>% 
-  group_by(per) %>% 
+  group_by(perc) %>% 
   filter(!is.na(value)) %>% 
-  summarise(prev.infected=round(mean(value/2000549)*100,4))
+  summarise(prev.infected=round(mean(value/2000000)*100,4))
 
 df %>% 
-  group_by(year(df$modSIRVsd.month), per) %>% 
+  group_by(year(df$modSIRVsd.month),group) %>% 
+  filter(!is.na(value)) %>% 
+  summarise(inf=sum(value))
+
+df %>% 
+  group_by(year(df$modSIRVsd.month), variable) %>% 
   filter(!is.na(value)) %>% 
   summarise(prev.infected=round(mean(value/2000549)*100,4))
 
@@ -266,7 +329,7 @@ modSIRVsd %>%
   summarise(total.infected=mean(infected))
 
 
-
+# modresults_before <-modresults
 
  # Grafico dos infectados ----
 # GGPLOT
@@ -400,116 +463,3 @@ sum(Fig25$Casos[1095:1395], na.rm = TRUE)
 102/530.8 # 19.2% system sentitivity
 
 (530.8/2044000)*100 # 0.025% prevalence (round)
-
-
-
-
-
-
-
-#############################################3
-
-##################### GREY ####################
-
-tiff(filename="0_SIRV_SIRVip_2014-2017_OIE_B&W.tif", width=200, height=180, pointsize=12,
-     units="mm", res=300, compression="lzw")
-
-plot(modSIRVsd$time, modSIRVsd$S, col="grey", lty= 1, lwd=2.5, type="l", 
-     ylim=c(0,560000), xlab="Time (days)", ylab = "Population")
-lines(modSIRVsd$time, modSIRVsd$I, col="black", lty= 1, lwd=3.5)
-lines(modSIRVsd$time, modSIRVsd$P, col="grey", lty= 1, lwd=2.5)
-lines(modSIRVsd$time, modSIRVsd$R, col="black", lty= 3, lwd=3.5)
-lines(modSIRVsd$time, modSIRVsd$V, col="grey", lty= 8, lwd=3.5)
-# lines(modSIRVsd$time, modSIRVsd$N, col="black")
-title(main = "SIRV CSFV 2014 - 2017 in Ecuador", sub = "Vaccine coverage simulation")
-grid (NULL,NULL, lty = 3, col = "cornsilk2")
-# legend("topright", legend=c("S"," I","P","R","V"), lty=1, cex=0.95, col=c("blue","red", "pink", "green","grey"))
-
-dev.off()
-
-# Looking for the infected over time ----
-Fig25 <- read.csv2(file="Beta_2014-2017_diario_Adjustment.csv", header=FALSE, sep=";")
-View(Fig25)
-
-Fig25$month <- seq(as.Date("2014-01-01"), as.Date("2017-10-26"), by = "days")
-
-afected2014 <- Fig25 %>% 
-  group_by(Month=floor_date(month, unit="month" )) %>% 
-  summarise(afectados=(sum(V2, na.rm=TRUE)))
-
-# Reading the affected
-afec <- read_csv("afectados.2014-2021.csv")
-
-afected <- rbind(afected2014[1:7,], afec[,c(2,3)])
-
-plot(afectados$Month, afectados$afectados)
-
-tiff(filename="1_Infectados_&_pI_2014-2017_2_OIE.tif", width=200, height=180, pointsize=12,
-     units="mm", res=300, compression="lzw")
-
-
-plot(afected$afectados, ylim=c(0,85000), pch=1, cex=0.7,
-     xlab = "Time (days)",
-     ylab = "Number of Cases")
-lines(modSIRVsd$time, modSIRVsd$I, lty=1)
-lines(modSIRVsd$time, modSIRVsd$PI, lty=2)
-
-title(main = "SIRV CSFV 2014 - 2017 in Ecuador", sub = ("Infected and persistent infection interraction"))
-grid (NULL,NULL, lty = 3, col = "cornsilk2")
-legend("topright", legend=c("I","pI"), lty=c(1, 2))
-
-dev.off()
-
-
-# Zoom infected ----
-plot(Fig25, ylim=c(0,1000), pch=1, cex=0.7,
-     xlab = "Time (days)",
-     ylab = "Number of Cases")
-lines(modSIRVsd$time, modSIRVsd$I, col="red")
-lines(modSIRVsd$time, modSIRVsd$PI, col="pink")
-
-Fig25 <- read.csv2(file="Infected.csv", header=FALSE, sep=";")
-names(Fig25) <- c("Tempo","Casos")
-
-
-
-# Grafico dos infectados zoom GREY ----
-tiff(filename="2_Infectados_&_pI_2014-2017_2_OIE.tif", width=200, height=180, pointsize=12,
-     units="mm", res=300, compression="lzw")
-
-Fig25 <- read.csv2(file="Beta_2014-2017_diario_Adjustment.csv", header=FALSE, sep=";")
-names(Fig25) <- c("Tempo","Casos")
-
-plot(Fig25, ylim=c(0,800), pch=1,
-     xlab = "Time (days)",
-     ylab = "Number of Cases")
-lines(modSIRVsd$time, modSIRVsd$I, col="black", lty=1, lwd=3.5)
-lines(modSIRVsd$time, modSIRVsd$PI, col="grey", lty=2, lwd=3.5)
-
-# title(main = "SIRV CSFV 2014 - 2017 in Ecuador", sub = ("Infected and persistent infection interraction"))
-grid (NULL,NULL, lty = 3, col = "cornsilk2")
-# legend("topright", legend=c("I","pI"), col=c("red", "pink"), cex=1.2, lty=c(1, 2))
-
-dev.off()
-
-##############
-#Recovered compartment----
-tiff(filename="3_Recovered_2014-2017.tif", width=200, height=180, pointsize=12,
-     units="mm", res=300, compression="lzw")
-
-Fig25 <- read.csv2(file="Beta_2014-2017_diario_Adjustment.csv", header=FALSE, sep=";")
-names(Fig25) <- c("Tempo","Casos")
-
-plot(Fig25, ylim=c(0,550000), pch=1,
-     xlab = "Time (days)",
-     ylab = "Number of Cases")
-lines(modSIRVsd$time, modSIRVsd$I, col="red")
-lines(modSIRVsd$time, modSIRVsd$PI, col="pink")
-lines(modSIRVsd$time, modSIRVsd$R, col="green")
-
-title(main = "Infected, persistent I & Recovered CSFV 2014 - 2017 in Ecuador", sub = ("Infected and persistent infection interraction"))
-grid (NULL,NULL, lty = 3, col = "cornsilk2")
-legend("topright", legend=c("R","I","pI"), lty=1, cex=0.95, col=c("green","red", "pink"))
-
-
-dev.off()
